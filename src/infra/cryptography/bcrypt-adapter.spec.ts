c27f743b5a449678509f297c3a6ce2e4252ea06e
Adilson <afs.adilson@gmail.com>
2021-04-02 11:47:58 -0300

    test: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -53,18 +53,9 @@ describe('Bcrypt Adapter', () => {
 })
 
 describe('Bcrypt Adapter', () => {
-  test('Should return true on compare success', async () => {
+  test('Should return true when compare succeeds', async () => {
     const sut = makeSut()
-    const hash = await sut.hash('any_value')
-    expect(hash).toBe('hash')
-  })
-})
-
-describe('Bcrypt Adapter', () => {
-  test('Should throw if hash compare throws', async () => {
-    const sut = makeSut()
-    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const promise = sut.hash('any_value')
-    await expect(promise).rejects.toThrow()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
   })
 })
