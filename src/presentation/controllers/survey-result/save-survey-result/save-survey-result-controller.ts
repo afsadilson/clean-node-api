64a50f57def98ff90811bbdfd2bed9970bc6886c
Adilson <afs.adilson@gmail.com>
2021-04-17 15:11:54 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -4,22 +4,34 @@ import {
   Controller,
   HttpRequest,
   HttpResponse,
-  LoadSurveyById
+  LoadSurveyById,
+  SaveSurveyResult
 } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
+      const { accountId } = httpRequest as { accountId: string }
       const { answer } = httpRequest.body
       const survey = await this.loadSurveyById.loadById(surveyId)
+
       if (survey) {
         const answers = survey.answers.map(a => a.answer)
         if (!answers.includes(answer)) {
           return forbidden(new InvalidParamError('answer'))
         }
+        await this.saveSurveyResult.save({
+          surveyId,
+          accountId,
+          answer,
+          date: new Date()
+        })
         return ok(survey)
       } else {
         return forbidden(new InvalidParamError('surveyId'))
