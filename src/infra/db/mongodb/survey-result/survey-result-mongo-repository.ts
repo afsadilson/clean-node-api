820365abac4aa470f912784a1ee1905ed837d10b
Adilson <afs.adilson@gmail.com>
2021-04-24 19:34:50 -0300

    test: ensure SurveyResultMongoRepository returns null on load survey without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -186,6 +186,6 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository, 
       })
       .build()
     const surveyResult = await surveyResultCollection.aggregate(query).toArray()
-    return surveyResult?.length ? surveyResult[0] : null
+    return surveyResult.length ? surveyResult[0] : null
   }
 }
